e7d00a2ecea8a50b52385522e8971bcc9acc79ee
fubin <fubin199410@qq.com>
2018-06-25 20:34:47 +0800

    加入显示天气信息的功功能

---
+++ app/src/main/java/android/coolweather/com/android/gson/AQI.java
@@ -0,0 +1,10 @@
+package android.coolweather.com.android.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
